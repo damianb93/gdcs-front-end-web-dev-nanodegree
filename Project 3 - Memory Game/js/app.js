525aa19872ade7471b9190a3b51b5980268a287b
damianb93 <damianburak93@gmail.com>
2018-05-13 21:20:14 +0200

    feat: Add comments to app.js

--- Project 3 - Memory Game/js/app.js
+++ Project 3 - Memory Game/js/app.js
@@ -21,18 +21,21 @@ let startTime = 0;
 let interval;
 
 /*-----LISTENERS-----*/
+
+/* Intialazing game state after document load*/
 document.addEventListener('DOMContentLoaded', function() {
     initGameState();
     initStars();
 });
 
+/* Main listener of the game, reacts on users click and handles main game logic */
 gameboard.addEventListener('click', function(evt){
     /*  isBetween(clickCount, 0, 1) locks flipping more than 2 cards at a time
         isCard(evt.target.nodeName) checks if clicked element is a card (cards are <li> elements)
         !isPickedOrmatched checks if clicked card was already picked or matched if so then the click is ignored   
     */
     if (isBetween(clickCount, 0, 1) && isCard(evt.target.nodeName) && !isPickedOrMatched(evt.target)) {
-        //Starts the timer at first card clicks
+        /* Starts the timer at first card click */
         if (startTime === 0) {
             startTime = new Date();
             interval = setInterval(function(){
@@ -44,30 +47,36 @@ gameboard.addEventListener('click', function(evt){
         pickedCards.push(card);
         clickCount++;
 
-        card.classList.toggle('rotate');
+        
+        card.classList.toggle('rotate'); // Rotates card to the front
         card.style.backgroundColor = 'rgba(0,0,0,0.5)';
         card.style.cursor = 'default';
 
+        // If pair of cards is selected
         if (clickCount === 2) {
             moves++;
             movesSpan.textContent = moves;
 
+            /* Rating logic */
             if (moves === 15) {
                 emptyStar(2);
             } else if (moves === 21) {
                 emptyStar(1);
             }
-            
+
             arePickedCardsEqual() ? markPickedCardsAsMatched() : resetPickedCards();
         }
     }
 });
 
+/* Main screen reset button click listener */
 restartBtn.addEventListener('click', function() {
     restartGame();
 });
 
+/* Summary screen reset button click listener */
 summaryRestartBtn.addEventListener('click', function() {
+    /* Changes current view from summary to game */
     if (mainContainer.style.display === 'none') {
         mainContainer.style.display = 'block';
         summaryContainer.style.display = 'none';
@@ -77,12 +86,14 @@ summaryRestartBtn.addEventListener('click', function() {
 })
 
 /*-----FUNCTIONS-----*/
+
+/* Randomizes and sets cards on board */
 function initGameState() {
     randomizeCards(cards);
     setOrRemoveCards();
 }
 
-//Fisher–Yates Shuffle
+// Fisher–Yates Shuffle
 function randomizeCards(array) {
     let counter = array.length;
 
@@ -99,18 +110,22 @@ function randomizeCards(array) {
     return array;
 }
 
+/* Checks if the given value is between given min and max */
 function isBetween(value, min, max) {
     return value >= min && value <= max;
 }
 
+/* Checks if the given element is a card (li element) */
 function isCard(elem) {
     return elem === 'LI';
 }
 
+/* Checks if the card was already picked or already has a match */
 function isPickedOrMatched(card) {
     return pickedCards.includes(card) || matchedCards.includes(card);
 }
 
+/* Checks if the currently picked pair of card is equal */
 function arePickedCardsEqual() {
     const firstCard = pickedCards[0].lastElementChild.firstElementChild;
     const secondCard = pickedCards[1].lastElementChild.firstElementChild;
@@ -118,12 +133,13 @@ function arePickedCardsEqual() {
     return firstCard.classList.contains(secondCard.classList.item(1));
 }
 
+/* Calculates elapsed time in seconds since the begining of the game */
 function calculateElapsedTime(endTime) {
     const timeDiff = (endTime - startTime)/1000;
     return Math.round(timeDiff);
 }
 
-// Sets/removes icon classes for cards
+// Sets/removes icon classes for/from cards
 function setOrRemoveCards() {
     let index = 0;
     document.querySelectorAll('.card i').forEach(e => {
@@ -132,6 +148,7 @@ function setOrRemoveCards() {
     });
 }
 
+/* Intializes scoreboard with full star icons */
 function initStars() {
     starsContainers.forEach(e => {
         for (star of e.children) {
@@ -140,6 +157,7 @@ function initStars() {
     })
 }
 
+/* Removes stars icons from scoreboard */
 function removeStars() {
     starsContainers.forEach(e => {
         for (star of e.children) {
@@ -148,6 +166,7 @@ function removeStars() {
     })
 }
 
+/* Removes full star and sets in its place empty one */
 function emptyStar(index) {
     starsContainers.forEach(e => {
         if (index >= 0 && index <= e.children.length) {
@@ -158,18 +177,23 @@ function emptyStar(index) {
     })
 }
 
+/* Sets full star icon for given element */
 function setFullStar(star) {
     star.classList.add('fas', 'fa-star');
 }
 
+/* Sets empty star icon for given element */
 function setEmptyStar(star) {
     star.classList.add('far', 'fa-star');
 }
 
+/* Removes class attribute from given element */
 function removeStarClass(star) {
     star.removeAttribute('class');  
 }
 
+/* Pushes currently picked cards to matchedCards array and resets clickCount and pickedCards array
+   If the game is over (all pairs are matched) invokes summaryGame method */
 function markPickedCardsAsMatched() {
     pickedCards.forEach(e => {
         e.classList.add('match');
@@ -186,13 +210,15 @@ function markPickedCardsAsMatched() {
     }
 }
 
+/* Informs the user of incorrect attempt by shaking card and changing background color to red
+   Resets selected cards state */
 function resetPickedCards() {
     pickedCards.forEach(e => {
         e.classList.add('shake');
         e.style.backgroundColor = 'rgba(255,0,0,0.8)'; // Sets cards background to red (wrong attempt)
     })
 
-    // Flips cards back to initial position, resets clickCount and pickedCards array
+    // Flips cards back to initial position, resets background color, clickCount variable and pickedCards array
     window.setTimeout(function(){
         pickedCards.forEach(e => {
             e.classList.toggle('rotate');
@@ -206,6 +232,7 @@ function resetPickedCards() {
     }, 650);
 }
 
+/* Changes view from game to summary */
 function summaryGame(finalMoves, finalTime) {
     mainContainer.style.display = 'none';
     summaryContainer.style.display = 'block';
@@ -214,8 +241,9 @@ function summaryGame(finalMoves, finalTime) {
     finalTimerSpan.textContent = finalTime + 's';
 }
 
+/* Resets game state to initial */
 function restartGame() {
-    // Sets intial position of all cards on board
+    // Flips back all cards on board
     document.querySelectorAll('.card').forEach(e => {
         if (e.classList.contains('rotate')) {
             e.classList.toggle('rotate');
@@ -229,8 +257,8 @@ function restartGame() {
     })
 
     window.setTimeout(function() {    
-        setOrRemoveCards(); // Remove existing cards
-        initGameState();
+        setOrRemoveCards(); // Removes existing cards
+        initGameState(); // Randomizes and sets cards on board
     }, 600);
 
     moves = 0;
@@ -238,6 +266,7 @@ function restartGame() {
     startTime = 0;
     timerSpan.textContent = 0;
 
+    /* Resets game state variables */
     clickCount = 0;
     pickedCards = [];
     matchedCards = [];
